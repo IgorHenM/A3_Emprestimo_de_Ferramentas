235b7935c8c849a83dcb2f0a2b953bd1b91f159d
Bruno Ambrozio <108434330+Fuinha-Raivosa@users.noreply.github.com>
2024-05-28 20:54:37 -0300

    Add files via upload
    Altera Registro de ferramenta no banco de dados como: marca, nome e custo.

---
+++ a3_programacao/src/main/java/files/DAO/Gerenciamento_Emprestimos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Gerenciamento;
+
+/**
+ *
+ * @author bruno
+ */
+public class Gerenciamento_Emprestimos {
+    
+}

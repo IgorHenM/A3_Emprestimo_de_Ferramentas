4b48ec84fe567ab06e874e6c4c537d1e98ad6f4e
IgorHenM <igormori16@gmail.com>
2024-06-15 17:26:13 -0300

    feat: correção de referência de imagens

--- src/main/java/files/ImagensA3/Imagens.java
+++ src/main/java/files/ImagensA3/Imagens.java
@@ -7,21 +7,21 @@ import javax.swing.ImageIcon;
 public class Imagens {
 
     public ImageIcon getLogo() {
-        File imglog = new File("A3_Emprestimo_de_Ferramentas\\src\\main\\java\\files\\ImagensA3\\Logo3.png");
+        File imglog = new File("src\\main\\java\\files\\ImagensA3\\Logo3.png");
 
         ImageIcon imgg = new ImageIcon(imglog.getPath());
         return imgg;
     }
 
     public ImageIcon getTools() {
-        File imglog = new File("A3_Emprestimo_de_Ferramentas\\src\\main\\java\\files\\ImagensA3\\Ferramentas.png");
+        File imglog = new File("src\\main\\java\\files\\ImagensA3\\Ferramentas.png");
 
         ImageIcon imgg = new ImageIcon(imglog.getPath());
         return imgg;
     }
 
     public ImageIcon getFriends() {
-        File imglog = new File("A3_Emprestimo_de_Ferramentas\\src\\main\\java\\files\\ImagensA3\\Amigos.png");
+        File imglog = new File("src\\main\\java\\files\\ImagensA3\\Amigos.png");
 
         ImageIcon imgg = new ImageIcon(imglog.getPath());
         return imgg;

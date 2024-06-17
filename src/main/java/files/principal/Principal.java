bde8d62507a747c0e3bd69504b20a9d849d7f9aa
IgorHenM <igormori16@gmail.com>
2024-06-17 09:51:51 -0300

    feat: Adição de comentários nos métodos

--- src/main/java/files/principal/Principal.java
+++ src/main/java/files/principal/Principal.java
@@ -1,6 +1,10 @@
 package files.principal;
 import files.view.TelaPrincipal;
 
+/*
+ * Método main responsável por rodar o programa
+ */
+
 public class Principal {
     public static void main(String[] args) {
         new TelaPrincipal().setVisible(true);
